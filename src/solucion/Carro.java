c3239e29184a15879b29df6dff9a4a4d5e2420d1
keny0z <kevinospinacdn@gmail.com>
2023-01-13 15:46:32 -0500

    ejemplo terminado

---
+++ src/solucion/Carro.java
@@ -0,0 +1,11 @@
+package solucion;
+
+public class Carro implements Vehiculo{
+
+	@Override
+	public void dibujar() {
+		System.out.println("dibujo de un carro");
+		
+	}
+
+}
---
+++ src/solucion/Moto.java
@@ -0,0 +1,11 @@
+package solucion;
+
+public class Moto implements Vehiculo{
+
+	@Override
+	public void dibujar() {
+		System.out.println("dibujo de una moto");
+		
+	}
+
+}
